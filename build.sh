a269ed09c2816ce65c0efdbb5328759509b573e4
haoxingsen <276843248@qq.com>
2021-03-05 15:29:57 +0800

    其他脚本增加git代码更新功能

--- build.sh
+++ build.sh
@@ -1,9 +1,65 @@
+server=/root/Message-Board
+cd $server
+echo "git pull ......"
+git pull
+workon flask # 进入本地环境
+verion=$(git rev-parse HEAD)
+
 PID=""
+RestartServer=false
+status=$(grep $verion ./version.txt)
+if [ -z "$status" ]
+then
+    echo "版本更新,重新部署~"
+    echo "记录版本号...."
+    echo $verion > ./version.txt
+    RestartServer=true
+else
+    echo "版本未更新~"
+fi
+
+# restart server
+echo "检测服务是否运行..."
 PID=$(ps -ef | grep python | grep app.py | awk '{ print $2 }')
+if [ -z "$PID" ]
+then
+    echo "服务未运行...."
+else
+    echo "服务运行中...."
+    if [ "$RestartServer"==true ]
+    then
+        echo "删除服务...."
+        kill -9  $PID
+        echo "killed "+$PID+""
+    fi
+fi
+
+PID=$(ps -ef | grep python3 | grep manage.py | awk '{ print $2 }')
+if [ -z "$PID"]
+then
+    python app.py >> log.log 2>&1 &
+    echo "启动服务...."
+fi
+echo "服务正在运行...."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if [ -z "$PID" ]
 then
-    echo "running python server not found "
+    echo "running python server"
 else
     #echo kill $PID
     kill -9  $PID
